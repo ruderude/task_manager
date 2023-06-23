ccba1d5faf2022c314d4c95df56fa359512833cd
ruderude <rude1979@gmail.com>
2023-06-24 03:53:23 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
 const nextConfig = {
+  swcMinify: true,
+  images: {
+    domains: ['cover.openbd.jp'],
+  },
   reactStrictMode: true,
-}
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = () => {
+  const plugins = [withPWA];
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+  return config;
+};
