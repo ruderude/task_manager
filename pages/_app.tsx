20c957cbd60e20e1e20971eb710fe3447b9dcced
ruderude <rude1979@gmail.com>
2023-06-24 10:22:36 +0900

    www

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.css'
 import '@/styles/destyle.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+  return (<SessionProvider session={session}><Component {...pageProps} /></SessionProvider>);
 }
