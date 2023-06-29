ffb4aaee7d6b49acf70547ff3515118e2d830037
ruderude <rude1979@gmail.com>
2023-06-29 11:05:55 +0900

    qqq

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,80 @@
-'use client'
+import React, { useState, useEffect } from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react'
+import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 
-import { useEffect } from 'react';
+export default function Home() {
+  const { data: session } = useSession({ required: true })
 
-function MyComponent() {
   useEffect(() => {
-    // コンポーネントがマウントされた後の処理
-    console.log('Component mounted');
+    console.log('useEffect')
+    console.log('session', session)
+    const email = session?.user?.email ?? ''
+    const protocol = window.location.protocol
+    const host = window.location.host
+    const url = protocol + "//" + host
+    const params = {email : email};
+    const query = new URLSearchParams(params)
+    const getAllUrl = url + `/api/task/all?${query}`
+    
+    const fetchAllTask = async () => {
+      try {
+        const res = await fetch(getAllUrl)
+        if (res.ok) {
+          const fetchData = await res.json()
+          console.log(fetchData)
+        }
+      } catch (error) {
+        console.error({ error })
+      } finally {
+        console.log('done')
+      }
+    }
+    fetchAllTask()
+  }, [session])
 
-    // コンポーネントがアンマウントされる前の処理
-    return () => {
-      console.log('Component unmounted');
-    };
-  }, []); // 第二引数に空の依存配列を指定することで、マウント時にのみ実行されます
+  if (!session) {
+    return <h1>Now Loading.....</h1>
+  }
 
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Task Manager</title>
+        <meta name="description" content="Task Manager" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <h1>Topページ</h1>
+          {/* <h2>ようこそ, {name}</h2>
+          <div>{email}</div>
+          <div>{image}</div>
+          {
+            <div>
+              <Image src={image} alt="" width={96} height={96} />
+            </div>
+          } */}
+          <div>
+            <Link href={`/previous`}>過去のタスク</Link>
+          </div>
+          <button onClick={() => signOut()}>Sign out</button>
+          {/* {allTask ?
+            allTask.map((task: any) => {
+              return (
+                <div key={task.id}>
+                  <div>{task.title}</div>
+                </div>
+              )
+            })
+            :
+            <div>タスクがありません</div>
+          } */}
+        </div>
+      </main>
+    </>
+  )
 }
-
-export default MyComponent;
