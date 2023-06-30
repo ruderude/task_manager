ed2394349f511492d3974bd91c9a1a31d6105197
ruderude <rude1979@gmail.com>
2023-06-30 16:13:19 +0900

    ddd

--- pages/api/task/create.ts
+++ pages/api/task/create.ts
@@ -32,7 +32,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     task: task,
   }
 
-  res.status(200).json(req.body)
+  res.status(200).json(req)
 }
 
 export default handler
