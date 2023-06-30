fe2a5daca4948c0d130ecef5ac3d459f88fc6878
ruderude <rude1979@gmail.com>
2023-07-01 04:08:06 +0900

    www

--- pages/index.tsx
+++ pages/index.tsx
@@ -251,7 +251,7 @@ export default function Home() {
                     tasks.map((task: TaskProps) => {
                       return (
                         <div key={task.id}>
-                          <div className={`${task.done ? "done" : ""} ${styles.task_title}`}>{task.title}</div>
+                          <div className={`${task.done ? styles.done : ""} ${styles.task_title}`}>{task.title}</div>
                           <div className={styles.task_under}>
                             <div className={styles.task_created}>{setDateString(task.createdAt)}</div>
                             <div className={styles.task_btn_area}>
